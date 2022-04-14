696a360649277fc2555d6d8edf7e8b9c35702da5
Ruslan Ishemgulov <ruslan.ishemgulov@gmail.com>
2022-04-14 11:25:44 +0200

    Frontend: install requirements
    * add webpack-encore tools
    * add vue/sass loaders
    * add default controller and template

---
+++ assets/js/components/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>It works!</h3>
+</template>
+
+<script>
+export default {
+  name: "MainPage"
+}
+</script>
+
+<style scoped>
+
+</style>
--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,9 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
